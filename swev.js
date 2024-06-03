2bd7d563bea2b0282840e63bc28597ec920a34a2
sebastiandahiya <135331602+sebastiandahiya@users.noreply.github.com>
2024-06-03 22:40:47 +0530

    Add files via upload

---
+++ swev.js
@@ -0,0 +1,12 @@
+gsap.from(".heading , .button" ,{
+    duration: 2,
+    ease: "power4.out",
+    x:200 ,
+   
+})
+gsap.from(".nav" ,{
+    duration: 4,
+    ease: "power4.out",
+    y:-200 ,
+   
+})
